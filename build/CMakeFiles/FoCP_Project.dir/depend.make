394a5a2b91ce6d242d899cd56ff7c750c782a633
Sheharyar-Khalid2006 <mohmmadsheheryar006@gmail.com>
2025-12-21 22:42:18 +0500

    removed CMakeLists.txt and other things

--- build/CMakeFiles/FoCP_Project.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for FoCP_Project.
-# This may be replaced when dependencies are built.
--- src/mainwindow.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef MAINWINDOW_HPP
-#define MAINWINDOW_HPP
-
-#include <QWidget>
-
-// Function that creates and returns the main window
-QWidget* createMainWindow();
-
-#endif // MAINWINDOW_HPP
